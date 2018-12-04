8de82e17e91c82af4d0682d0a85b3a8cb1fdf91c
Kim Eggler <kim.eggler@gmail.com>
2018-12-04 14:10:10 +0100

    Initial Commit

---
+++ views/home.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: begglk
+ * Date: 04.12.2018
+ * Time: 11:48
+ */
+?>
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,400,700" rel="stylesheet">
